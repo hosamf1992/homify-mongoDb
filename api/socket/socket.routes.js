39271b52f3104d7973ebecdedab090b04556b5f4
Shachal Ami-Ad <shachalamiad@hotmail.com>
2019-12-01 23:20:22 +0200

    socket

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -3,18 +3,30 @@ module.exports = connectSockets
 
 function connectSockets(io) {
     io.on('connection', socket => {
-        socket.on('chat newMsg', msg=>{
-            console.log(msg)
+        // socket.on('chat newMsg', msg=>{
+        //     console.log(msg)
             // io.emit('chat addMsg', msg)
             // emits only to sockets in the same room
-            io.to(socket.myTopic).emit('chat addMsg', msg)
+            // io.to(socket.myTopic).emit('chat addMsg', msg)
+        // })
+        // socket.on('chat topic', topic=>{
+        //     if (socket.myTopic) {
+        //         socket.leave(socket.myTopic)
+        //     }
+        //     socket.join(topic)
+        //     socket.myTopic = topic;
+        // })
+        socket.on('order details', order=>{
+            console.log(order)
+            io.emit('get order details', order)
         })
-        socket.on('chat topic', topic=>{
-            if (socket.myTopic) {
-                socket.leave(socket.myTopic)
-            }
-            socket.join(topic)
-            socket.myTopic = topic;
+        socket.on('approve order', order=>{
+            console.log('approved')
+            io.emit('approve order', order)
+        })
+        socket.on('reject order', order=>{
+            console.log('rejected')
+            io.emit('reject order', order)
         })
     })
 }
