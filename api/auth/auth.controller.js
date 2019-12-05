70ee179cb3a46503a77c953f38da4181583be4c5
hosam faour <hosamf1992@gmail.com>
2019-12-05 10:59:31 +0200

    fix logger

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -15,14 +15,14 @@ async function login(req, res) {
 async function signup(req, res) {
     try {
         const { email, password, username,img } = req.body
-        logger.debug(email + ", " + username + ', ' + password)
+        // logger.debug(email + ", " + username + ', ' + password)
         const account = await authService.signup(email, password, username,img)
-        logger.debug(`auth.route - new account created: ` + JSON.stringify(account))
+        // logger.debug(`auth.route - new account created: ` + JSON.stringify(account))
         const user = await authService.login(email, password)
         req.session.user = user
         res.json(user)
     } catch (err) {
-        logger.error('[SIGNUP] ' + err)
+        // logger.error('[SIGNUP] ' + err)
         res.status(500).send({ error: 'could not signup, please try later' })
     }
 }
