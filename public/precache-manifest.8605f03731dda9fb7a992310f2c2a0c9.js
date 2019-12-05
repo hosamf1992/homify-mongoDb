708cd774a0bb7df1093bbf9cb55aead9378d4bcb
hosam faour <hosamf1992@gmail.com>
2019-12-05 09:56:12 +0200

    heroku

---
+++ public/precache-manifest.8605f03731dda9fb7a992310f2c2a0c9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca97cdc9866086e122e5",
+    "url": "/css/app.5b72e94c.css"
+  },
+  {
+    "revision": "9b02592bacd6a42f50fc",
+    "url": "/css/chunk-vendors.6bd32be9.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "c627a7b9b0580ee287f7462547a0681c",
+    "url": "/img/Amit Fortus.c627a7b9.jpeg"
+  },
+  {
+    "revision": "85ae8faf1cb4f3f95856f317c1debc43",
+    "url": "/img/Hosam Faour.85ae8faf.jpeg"
+  },
+  {
+    "revision": "78d6ddc0d494b54178194a60413d6c6e",
+    "url": "/img/Shachal Ami-Ad.78d6ddc0.jpeg"
+  },
+  {
+    "revision": "bedf96769d8537ef6a826ee54c6cc3ec",
+    "url": "/img/bulb.bedf9676.png"
+  },
+  {
+    "revision": "c278f85272c505b689ecfe500031c1e2",
+    "url": "/img/logo.c278f852.png"
+  },
+  {
+    "revision": "ba6c63d91be2a02e3da691a87d3aec7c",
+    "url": "/img/star.ba6c63d9.png"
+  },
+  {
+    "revision": "ba9e022c1a85e27e20a6ecb608e2e99b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca97cdc9866086e122e5",
+    "url": "/js/app.963dbe13.js"
+  },
+  {
+    "revision": "9b02592bacd6a42f50fc",
+    "url": "/js/chunk-vendors.f0cd149d.js"
+  },
+  {
+    "revision": "e373f86d39b537ff28fd60ca4bb75c88",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
